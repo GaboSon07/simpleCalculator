13d2d8970679c3095023844490d69cca5ec85018
Gabriel J. Cedeño <cedenodiazgabriel@gmail.com>
2026-05-02 16:54:25 -0300

    Refactoring repetitive code
    Since there were lines with the same code I tried to create a function
    that could contain them all and do the same job with less verbose... And
    I could.
    
    Perhaps I am being too repetitive myself by typing "refactoring"
    alongside "repetitive", but it reminds me what I did. Nevertheless, it
    is being an excellent way to practice and make mistakes.
    
    Thank you for reading this, it means it reached someone aside from
    myself.

--- src/App.java
+++ src/App.java
@@ -9,55 +9,47 @@ public class App {
     System.out.println(t);
 	}
 
-    public static void simpleCalculator(){
-        Scanner scan = new Scanner(System.in);    
-				int n2 = 0;
-                int result = 0;
-                print("Type in your first number: ");
-                result = scan.nextInt();
-                scan.nextLine();
-        while (true){
-			print("Insert a valid selector (+, -, *, /): ");
-			String selector = scan.nextLine();
-            if (selector.equals("end")){
-				print("Calculator closed.");
-                break;
-            } else {
-				if (selector.equals("+")) {
-                    System.out.println("Your current number is: " + result);
-                    print("Type in the second value: ");
-                    n2 = scan.nextInt();
-                    scan.nextLine();
-                    System.out.println("The result is: "  + (result + n2));
-                    result += n2;
-                } else if (selector.equals("-")) {
-					System.out.println("Your current number is: " + result);
-                    print("Type in the second value: ");
-                    n2 = scan.nextInt();
-                    scan.nextLine();
-                    System.out.println("The result is: "  + (result - n2));
-                    result -= n2;
-				} else if (selector.equals("*")) {
-					System.out.println("Your current number is: " + result);
-                    print("Type in the second value: ");
-                    n2 = scan.nextInt();
-                    scan.nextLine();
-                    System.out.println("The result is: "  + (result * n2));
-                    result *= n2;
-				}  else if (selector.equals("/")) {
-					System.out.println("Your current number is: " + result);
-                    print("Type in the second value: ");
-                    n2 = scan.nextInt();
-                    scan.nextLine();
-                    System.out.println("The result is: "  + (result / n2));
-                    result /= n2;
-				} else {
-					print("A proper selector is to be typed in");
-				}
-            }
+public static int secondValue(int res, Scanner scan){  // This function helps not to repeat the same code more than once
+    System.out.println("Your current value is: " + res);
+    print("Type in the second value: ");
+    int num = scan.nextInt();
+        scan.nextLine();
+        return num;
+}
+
+public static void simpleCalculator(){
+    Scanner scan = new Scanner(System.in);    
+		int n2 = 0;
+        int result = 0; // By requering the first number before getting into the loop "while", the rest of the program can be run in the same way and there's no need to aks for it anymore
+        print("Type in your first number: ");
+        result = scan.nextInt();
+        scan.nextLine(); // It is always a good practice to add this line after using "nextInt()" so it does not throw an extra line at you while running the program
+    while (true){
+		print("Insert a valid selector (+, -, *, /): ");
+		String selector = scan.nextLine();
+        if (selector.equals("end")){
+			print("Calculator closed.");
+            break;
+        } else {
+			if (selector.equals("+")) {
+                n2 = secondValue(result, scan); // It is good summaring all the code on lines 12 through 18 in just one
+                System.out.println("The result is: "  + (result += n2)); // (+=) This is the reason why you can loop through the code almost infinitely
+            } else if (selector.equals("-")) {
+				n2 = secondValue(result, scan);
+                System.out.println("The result is: "  + (result -= n2));
+			} else if (selector.equals("*")) {
+				n2 = secondValue(result, scan);
+                System.out.println("The result is: "  + (result *= n2));
+			}  else if (selector.equals("/")) {
+				n2 = secondValue(result, scan);
+                System.out.println("The result is: "  + (result /= n2));
+			} else {
+				print("A proper selector is to be typed in \n"); // I must admit I do not know how to check whether what is being typed in is an integer or not, so I only check for the selector
+			}
         }
-        scan.close();
     }
+    scan.close();
+}
 
     public static void main(String[] args) throws Exception {
         simpleCalculator();
